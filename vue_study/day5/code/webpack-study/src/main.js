4ba8ac16d8735a3c0a8d4641f1bb5d002eab3547
xiaxixi <i995170241@163.com>
2019-04-07 23:55:49 +0800

    day5-code

---
+++ vue_study/day5/code/webpack-study/src/main.js
@@ -0,0 +1,24 @@
+// 这是 main.js 是我们项目的JS入口文件
+
+
+// 1. 导入 Jquery
+// import *** from *** 是ES6中导入模块的方式
+// 由于 ES6的代码，太高级了，浏览器解析不了，所以，这一行执行会报错
+import $ from 'jquery'
+// const $ = require('jquery')
+
+// 使用 import 语法，导入 CSS样式表
+import './css/index.css'
+import './css/index.less'
+import './css/index.scss'
+// 注意： webpack, 默认只能打包处理 JS 类型的文件，无法处理 其它的非 JS 类型的文件；
+// 如果要处理 非JS类型的文件，我们需要手动安装一些 合适 第三方 loader 加载器；
+// 1. 如果想要打包处理 css 文件，需要安装 cnpm i style-loader css-loader -D
+// 2. 打开 webpack.config.js 这个配置文件，在 里面，新增一个 配置节点，叫做 module, 它是一个对象；在 这个 module 对象身上，有个 rules 属性，这个 rules 属性是个 数组；这个数组中，存放了，所有第三方文件的 匹配和 处理规则；
+
+$(function () {
+    $('li:odd').css('backgroundColor', 'lightblue')
+    $('li:even').css('backgroundColor', function () {
+        return '#' + 'dddddd'
+    })
+})
